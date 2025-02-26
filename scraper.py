70210953114a5bd3809caaa02ccf14cae0f2bce4
Manish <manish@Manishs-MacBook-Air.local>
2025-02-26 14:44:26 +0530

    idletimeout fix and and catching exeption for gracefull error handling

--- scraper.py
+++ scraper.py
@@ -6,15 +6,15 @@ async def scrape(urls):
     results = []
 
     async with async_playwright() as p:
-        browser = await p.chromium.launch(executable_path="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",headless=True)
+        browser = await p.chromium.launch(headless=True)
         
         page = await browser.new_page()
 
         for url in urls:
-            await page.goto(url, wait_until="networkidle") # Ensure all network request are complete
-            await asyncio.sleep(2)  # Ensure all dynamic content is rendered (for React, Angular, etc.)
-
             try:
+                await page.goto(url, wait_until="networkidle") # Ensure all network request are complete
+                await asyncio.sleep(2)  # Ensure all dynamic content is rendered (for React, Angular, etc.)
+
                 # Wait for body to ensure full page load
                 await page.wait_for_selector("body", timeout=1000)
 
@@ -34,12 +34,6 @@ async def scrape(urls):
                 })
             except Exception as e:
                 print(f"Error scraping {url}: {e}")
-                results.append({
-                    "url": url,
-                    "title": None,
-                    "description": None,
-                    "text": None
-                })
 
         await browser.close()
     
